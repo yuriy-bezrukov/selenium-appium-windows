70e3a73280fd28aef9f36e168731eb2fef0a6700
bezrukov <bezrukov-yra@mail.ru>
2021-02-03 08:39:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Example to do Calulator automation on Windows
+1. Download and install WinAppDriver: https://github.com/microsoft/WinAppDriver/releases
+2. cd to example directory and install packages.
+
+`yarn install` or `npm install`
+
+3. start winappdriver or ywinappdriver
+
+`npx winappdriver` or `npx ywinappdriver`
+
+[WinAppDriver](https://github.com/microsoft/WinAppDriver) is supported by Microsoft.
+[YWinAppDriver](https://github.com/licanhua/YWinAppDriver) is an open source project which implemented WinAppDriver's functionality, and is developed by Canhua Li
+
+4. run jest test
+
+`yarn run jest` or `npm run jest`
---
+++ __tests__/By2.test.ts
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { By2, driver, WebDriver2 } from 'selenium-appium'
+import { Builder, until } from 'selenium-webdriver';
+import { capabilities } from '../Setup'
+
+jest.setTimeout(50000);
+
+const url = 'http://localhost:4723'
+
+describe('By2', () => {
+    test("By2 used in selenium-webdriver.WebDriver", async () => {
+        const webdriver = await new Builder()
+            .usingServer(url)
+            .withCapabilities(capabilities)
+            .build();
+        const element = await webdriver.wait(until.elementLocated(By2.nativeName('One')));
+        await element.click();
+        await webdriver.quit();
+    });
+
+    test("By2 Supports multiple webdriver2", async () => {
+        const webdriver = new WebDriver2();
+        await webdriver.startWithCapabilities(capabilities)
+        await By2.nativeName('One', webdriver).click();
+        await webdriver.quit();
+    });
+
+
+    test("By2 deduced WebDriver2 for single WebDriver2", async () => {
+        await driver.startWithCapabilities(capabilities)
+        await By2.nativeName('One').click();
+        await driver.quit();
+    });
+})
